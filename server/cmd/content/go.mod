ec2230a8fe10a3ddebf779f799467e06b23dbd9f
leedonggyu <zkfmapf123@naver.com>
2025-01-07 23:13:14 +0900

    wip-50ab0ca8

--- server/cmd/content/go.mod
+++ server/cmd/content/go.mod
@@ -40,6 +40,7 @@ require (
 	github.com/jcmturner/gofork v1.7.6 // indirect
 	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
--- server/cmd/content/main.go
+++ server/cmd/content/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"cmd/content/apis"
 	"cmd/content/middlewares"
+	"fmt"
+	mysql "internal/databases"
 	"internal/net"
+	"os"
 
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 )
 
 const (
@@ -13,8 +17,29 @@ const (
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	// Server
 	app := net.NewHTTP("core", "1.0.0")
 	app.Use(logger.New())
+
+	// Config : mysql
+	db, err := mysql.CreateDBConnection().
+		WithHost(os.Getenv("DB_HOST")).
+		WithPort(os.Getenv("DB_PORT")).
+		WithUser(os.Getenv("DB_USER")).
+		WithPassword(os.Getenv("DB_PASSWORD")).
+		WithDatabase(os.Getenv("DB_DATABASE")).
+		Build()
+	
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("db : ", db)
 
 	app.Get("/health",middlewares.LoggerMiddleware(), apis.HealthCheck())
 	net.Run(app, PORT)	
