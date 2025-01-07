6206d2e82fd6d2f768d3859054b09f25ff91243c
leedonggyu <zkfmapf123@naver.com>
2025-01-07 23:50:17 +0900

    Update main

--- server/cmd/content/main.go
+++ server/cmd/content/main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"cmd/content/apis"
 	"cmd/content/middlewares"
-	"fmt"
-	mysql "internal/databases"
 	"internal/net"
-	"os"
 
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
@@ -25,22 +22,7 @@ func main() {
 	// Server
 	app := net.NewHTTP("core", "1.0.0")
 	app.Use(logger.New())
-
-	// Config : mysql
-	db, err := mysql.CreateDBConnection().
-		WithHost(os.Getenv("DB_HOST")).
-		WithPort(os.Getenv("DB_PORT")).
-		WithUser(os.Getenv("DB_USER")).
-		WithPassword(os.Getenv("DB_PASSWORD")).
-		WithDatabase(os.Getenv("DB_DATABASE")).
-		Build() 
 	
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("db : ", db)
-
 	app.Get("/health",middlewares.LoggerMiddleware(), apis.HealthCheck())
 	net.Run(app, PORT)	
 }
